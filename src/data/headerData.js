7d61775643f359387c077ede28a751c0e6544e48
elizabethgenetugit config --global user.email elizabeth.genetu@gmail.com <abrishg73@gmail.com>
2025-06-24 01:23:09 -0500

    ff

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,5 +4,5 @@ export const headerData = {
     desciption: "Hello! This is  Elizabeth Genetu, an Addis-Coder 2023 Alumnus and aspiring computer engineer. A quick learner with a self-learning attitude. I love math, science, and exploring new technologies to solve problems.",
     image: 'https://i.ibb.co/BHSMcyWc/gggh.jpg',
     imagebw: 'https://i.ibb.co/BHSMcyWc/gggh.jpg',
-    resumePdf: 'https://drive.google.com/file/d/1Qa2YOlPNGLgnFhZqyZmNrvXJinDKU1SA/view?usp=sharing'
+    resumePdf: 'https://drive.google.com/file/d/1r1jmkfE_bUZPahpyJOzkkbXvoI3QDjVG/view?usp=sharing'
 }

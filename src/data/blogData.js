59a8392091eeaece6c2f5c404bb2535aa3a5cc27
elizabethgenetugit config --global user.email elizabeth.genetu@gmail.com <abrishg73@gmail.com>
2025-06-24 01:06:25 -0500

    bed bug

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,13 +1,13 @@
 const blogs = [
     {
-        id: 1,
-        title: 'Lemlem Academic Excellence Recognition',
-        description: "Recognition poster awarded for achieving 1st place among all Grade 10 sections at Lemlem School.",
-        date: 'May 2025',
-        image: '',
-        url: 'https://ibb.co/0VM6g6Nk'
-      },
-      {
+      id: 1,
+      title: 'Lemlem Academic Excellence Recognition',
+      description: "Recognition poster awarded for achieving 1st place among all Grade 10 sections at Lemlem School.",
+      date: 'May 2025',
+      image: '',
+      url: 'https://ibb.co/0VM6g6Nk'
+    },
+    {
       id: 2,
       title: 'First Addis Coder 2023 Batch Graduation',
       description: 'Celebrating the first graduation of the Addis Coder 2023 batch at Etege Menen Boarding School.',
@@ -15,7 +15,7 @@ const blogs = [
       image: '',
       url: 'https://i.ibb.co/vCSFMY5D/addis-coder-handing.jpg'
     },
-     {
+    {
       id: 3,
       title: 'Regional School Youth Summit, Bahir Dar',
       description: 'Participating in the Regional School Youth Summit in Bahir Dar, 2022.',
@@ -31,7 +31,6 @@ const blogs = [
       image: '',
       url: 'https://i.ibb.co/zTbSPDSS/addis-coder-1-together.jpg'
     },
-   
     {
       id: 5,
       title: 'Meeting Dr. Jelani Nelson',
@@ -39,7 +38,8 @@ const blogs = [
       date: 'August 2023',
       image: '',
       url: 'https://i.ibb.co/pj5K4rP6/photo-2023-08-11-08-19-32.jpg'
-    },
-    
+    }
   ];
+  
+  export default blogs;
   
--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -1,90 +1,38 @@
-import five from '../assets/svg/projects/five.svg'
 import four from '../assets/svg/projects/four.svg'
 import seven from '../assets/svg/projects/seven.svg'
+// import five from '../assets/svg/projects/five.svg'
 // import six from '../assets/svg/projects/six.svg'
 // import three from '../assets/svg/projects/three.svg'
 // import two from '../assets/svg/projects/two.svg'
 
-
 export const projectsData = [
-    {
-        id: 1,
-        projectName: 'AddisCoder X Programming',
-        projectDesc: 'AddisCoder X is a 3-week online computer science course of the 2023 AddisCoder Almunus, designed to share our fellow high school students to the exciting world of programming and problem-solving we learned from AddisCoder. ',
-        tags: ['Python', 'Data Structure', 'Algorithm'],
-        code: 'https://github.com/elizabethgenetu/AddisCoder_X',
-        demo: '',
-        image: seven
-    },
-    {
-        id: 2,
-        projectName: 'Portfolio',
-        projectDesc: 'This project is about creating personal portfolio to share my expereince and academic informations',
-        tags: ['React', 'Javascript'],
-        code: 'https://github.com/elizabethgenetu/elizabethgenetu',
-        demo: 'https://elizabethgenetu.netlify.app/',
-        image: four
-    },
-   
-    // {
-    //     id: 4,
-    //     projectName: 'Android Patient Tracker',
-    //     projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
-    //     tags: ['Flutter', 'Firebase'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: five
-    // },
-    // {
-    //     id: 5,
-    //     projectName: 'E-Commerce App',
-    //     projectDesc: 'A Simple E-commerce application',
-    //     tags: ['React Native', 'Firebase'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: five
-    // },
-    // {
-    //     id: 6,
-    //     projectName: 'Uber Lite',
-    //     projectDesc: 'Uber clone',
-    //     tags: ['Flutter'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: one
-    // },
-    // {
-    //     id: 7,
-    //     projectName: 'Stock Market App',
-    //     projectDesc: 'A simple stock market API app',
-    //     tags: ['React', 'Redux', 'Bootstrap'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: two
-    // },
-    // {
-    //     id: 8,
-    //     projectName: 'Car Pooling System',
-    //     projectDesc: 'The carpooling system merges multiple people in a car which leads to meet new people, reduces pollution',
-    //     tags: ['Flutter', 'React'],
-    //     code: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     demo: 'https://github.com/hhhrrrttt222111/developer-portfolio',
-    //     image: three
-    // },
-]
-
-
-// Do not remove any fields.
-// Leave it blank instead as shown below
-
-/* 
-{
+  {
     id: 1,
-    projectName: 'Car Pooling System',
-    projectDesc: '',
-    tags: ['Flutter', 'React'],
-    code: '',
+    projectName: 'AddisCoder X Programming',
+    projectDesc: 'AddisCoder X is a 3-week online computer science course of the 2023 AddisCoder Alumni, designed to introduce fellow high school students to the exciting world of programming and problem-solving we learned from AddisCoder.',
+    tags: ['Python', 'Data Structure', 'Algorithm'],
+    code: 'https://github.com/elizabethgenetu/AddisCoder_X',
     demo: '',
-    image: ''
-}, 
-*/
+    image: seven
+  },
+  {
+    id: 2,
+    projectName: 'Portfolio',
+    projectDesc: 'This project is about creating a personal portfolio to share my experience and academic information.',
+    tags: ['React', 'JavaScript'],
+    code: 'https://github.com/elizabethgenetu/elizabethgenetu',
+    demo: 'https://elizabethgenetu.netlify.app/',
+    image: four
+  }
+
+  // You can uncomment and clean these up as needed:
+  // {
+  //   id: 3,
+  //   projectName: 'Android Patient Tracker',
+  //   projectDesc: 'This project involves the development of an Android application for viewing and managing patient data.',
+  //   tags: ['Flutter', 'Firebase'],
+  //   code: '',
+  //   demo: '',
+  //   image: five
+  // },
+]

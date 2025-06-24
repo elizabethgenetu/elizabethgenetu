98c93b1894613af1efe13ed423fc5ff8e4eecfaf
elizabethgenetugit config --global user.email elizabeth.genetu@gmail.com <abrishg73@gmail.com>
2025-06-24 01:09:43 -0500

    d

--- src/data/blogData.js
+++ src/data/blogData.js
@@ -1,45 +1,44 @@
-const blogs = [
+export const blogData = [
     {
-      id: 1,
-      title: 'Lemlem Academic Excellence Recognition',
-      description: "Recognition poster awarded for achieving 1st place among all Grade 10 sections at Lemlem School.",
-      date: 'May 2025',
-      image: '',
-      url: 'https://ibb.co/0VM6g6Nk'
+        id: 1,
+        title: 'Lemlem Academic Excellence Recognition',
+        description: "Recognition poster awarded for achieving 1st place among all Grade 10 sections at Lemlem School.",
+        date: 'May 2025',
+        image: '',
+        url: 'https://ibb.co/0VM6g6Nk'
     },
     {
-      id: 2,
-      title: 'First Addis Coder 2023 Batch Graduation',
-      description: 'Celebrating the first graduation of the Addis Coder 2023 batch at Etege Menen Boarding School.',
-      date: 'August 2023',
-      image: '',
-      url: 'https://i.ibb.co/vCSFMY5D/addis-coder-handing.jpg'
+        id: 2,
+        title: 'First Addis Coder 2023 Batch Graduation',
+        description: 'Celebrating the first graduation of the Addis Coder 2023 batch at Etege Menen Boarding School.',
+        date: 'August 2023',
+        image: '',
+        url: 'https://i.ibb.co/vCSFMY5D/addis-coder-handing.jpg'
     },
     {
-      id: 3,
-      title: 'Regional School Youth Summit, Bahir Dar',
-      description: 'Participating in the Regional School Youth Summit in Bahir Dar, 2022.',
-      date: '2022',
-      image: '',
-      url: 'https://i.ibb.co/LDH1PSFq/regional-summit.jpg'
+        id: 3,
+        title: 'Regional School Youth Summit, Bahir Dar',
+        description: 'Participating in the Regional School Youth Summit in Bahir Dar, 2022.',
+        date: '2022',
+        image: '',
+        url: 'https://i.ibb.co/LDH1PSFq/regional-summit.jpg'
     },
     {
-      id: 4,
-      title: 'Life at Addis Coder 2023 Batch',
-      description: 'Experiencing hiking, visiting, and learning at Addis Coder 2023.',
-      date: '2023',
-      image: '',
-      url: 'https://i.ibb.co/zTbSPDSS/addis-coder-1-together.jpg'
+        id: 4,
+        title: 'Life at Addis Coder 2023 Batch',
+        description: 'Experiencing hiking, visiting, and learning at Addis Coder 2023.',
+        date: '2023',
+        image: '',
+        url: 'https://i.ibb.co/zTbSPDSS/addis-coder-1-together.jpg'
     },
     {
-      id: 5,
-      title: 'Meeting Dr. Jelani Nelson',
-      description: 'Inspired by the visionary computer scientist whose unwavering support helped us see opportunity and chase it. Thank you, Dr. Jelani!',
-      date: 'August 2023',
-      image: '',
-      url: 'https://i.ibb.co/pj5K4rP6/photo-2023-08-11-08-19-32.jpg'
+        id: 5,
+        title: 'Meeting Dr. Jelani Nelson',
+        description: 'Inspired by the visionary computer scientist whose unwavering support helped us see opportunity and chase it. Thank you, Dr. Jelani!',
+        date: 'August 2023',
+        image: '',
+        url: 'https://i.ibb.co/pj5K4rP6/photo-2023-08-11-08-19-32.jpg'
     }
-  ];
-  
-  export default blogs;
-  
+    ];
+    
+          
